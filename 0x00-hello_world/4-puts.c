06ec54783b7f4c046dcac6c0604f325bfb28a735
JoshAwuro <joshuawuro@gmail.com>
2023-02-16 20:51:57 -0800

    new

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/*
+*main - prints "programming is like building a multilingual puzzle, followed by a newlin."
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle, followed by a newline")
+	return (0);
+}
