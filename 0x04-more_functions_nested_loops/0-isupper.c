c9839c4dbaed9b287cb02bc9af0846caacee4bb1
JoshAwuro <joshuawuro@gmail.com>
2023-02-23 17:43:24 -0800

    add

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -11,8 +11,8 @@
 int isUpperCase(char c)
 {
     if (c >= 'A' && c <= 'Z') {
-        return 1; // capital letter
+        return 1; 
     } else {
-        return 0; // not a capital letter
+        return 0; 
     }
 }
